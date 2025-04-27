e35d8617830d5d9398fb8e338560e322348b15b3
Ashutosh Mishra <ashutm18802@gmail.com>
2025-04-27 16:15:11 +0530

    adding all the files in pages prj

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ClassDirective } from './class.directive';
+import { TimesDirective } from './times.directive';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports:[CommonModule, ClassDirective, TimesDirective]
 })
+
 export class AppComponent {
-  title = 'pages';
+  currentPage=0;
+  images=[
+    {
+      title:'Beach',
+      url:'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+      title:'Motorcycle',
+      url:'https://images.unsplash.com/photo-1572452571879-3d67d5b2a39f?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+      title:'Space',
+      url:'https://images.unsplash.com/photo-1419242902214-272b3f66ee7a?q=80&w=2113&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    },
+    {
+      title:'Airplane',
+      url:'https://images.unsplash.com/photo-1610642372651-fe6e7bc209ef?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    }
+  ]
+
 }
